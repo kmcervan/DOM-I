535101b08394569dddd65d5871181e7187e9dec0
Karla Cervantes <cervantes.karla14@gmail.com>
2020-11-30 21:55:21 -0600

    Finished Tasks

--- js/index.js
+++ js/index.js
@@ -40,3 +40,57 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Header
+const nav = document.querySelectorAll("a");
+const navArray = Array.from(nav);
+navArray[0].textContent = "Services";
+navArray[1].textContent = "Product";
+navArray[2].textContent = "Vision";
+navArray[3].textContent = "Features";
+navArray[4].textContent = "About";
+navArray[5].textContent = "Contact";
+
+// Section CTA
+const hOne = document.querySelector("h1");
+hOne.textContent = siteContent["cta"]['h1'];
+hOne.innerHTML = "DOM <br> IS <br> AWESOME";
+
+// Section CTA Image
+const ctaImage = document.querySelector("#cta-img");
+ctaImage.src = "img/header-img.png";
+
+// Section CTA Button
+const button = document.querySelector("button");
+button.textContent = "Get Started";
+
+// Main-content
+const hFour = document.querySelectorAll(".text-content h4");
+const fourArray = Array.from(hFour);
+hFour[0].textContent = "Features";
+hFour[1].textContent = "About";
+hFour[2].textContent = "Services";
+hFour[3].textContent = "Product";
+hFour[4].textContent = "Vision";
+const pText = document.querySelectorAll(".text-content p");
+const pArray = Array.from(pText);
+pText[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+pText[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+pText[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+pText[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+pText[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+const midImg = document.querySelector(".middle-img");
+midImg.src = "img/mid-page-accent.jpg";
+
+// Section Contact
+const hFourBot = document.querySelector(".contact h4");
+hFourBot.textContent = "Contact";
+const pBot = document.querySelectorAll(".contact p");
+const pBotArr = Array.from(pBot);
+pBot[0].innerHTML = "123 Way 456 Street<br>Somewhere, USA";
+pBot[1].textContent = "1 (888) 888-8888";
+pBot[2].textContent = "sales@greatidea.io";
+
+// Footer
+const footer = document.querySelector("footer p");
+footer.textContent = "Copyright Great Idea! 2018";
